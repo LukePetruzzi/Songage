b2af3180c355f2461e19f4d3ffc49f867fa4b117
Luke Petruzzi <lukepetruzzi@gmail.com>
2016-06-20 11:21:29 +0200

    first commit

---
+++ Songage/ChooseImageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ChooseImageViewController.swift
+//  Songage
+//
+//  Created by Luke Petruzzi on 6/20/16.
+//  Copyright © 2016 Luke Petruzzi. All rights reserved.
+//
+
+import UIKit
+
+class ChooseImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
