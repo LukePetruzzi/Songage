9def48b25ff0b07c1f644e79717d911166647155
Luke Petruzzi <lukepetruzzi@gmail.com>
2016-06-20 11:57:46 +0200

    working imagepickers

--- Songage/ChooseImageViewController.swift
+++ Songage/ChooseImageViewController.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 
-class ChooseImageViewController: UIViewController {
+class ChooseImageViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-    override func viewDidLoad() {
+    // create all outlets
+    @IBOutlet weak var imageView: UIImageView!
+    
+    // create an image picker to use when add photo button is tapped
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // set the delegate of the imagePicker
+        imagePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +29,79 @@ class ChooseImageViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func addImageButtonTapped(sender: UIButton)
+    {
+        // let the user edit the photo before adding if they want
+        imagePicker.allowsEditing = false
+        
+        let alert = UIAlertController(title: "New Songage", message: "Where do you want the image for your Songage to come from?", preferredStyle: .ActionSheet)
+        
+        let cameraAction = UIAlertAction(title: "Take a Photo", style: .Default, handler: {(cameraAction) -> Void in
+            
+            // take a photo
+            self.imagePicker.sourceType = .Camera
+            self.presentViewController(self.imagePicker, animated: true, completion: nil)
+        })
+        let libraryAction = UIAlertAction(title: "Photo from Library", style: .Default, handler: {(libraryAction) -> Void in
+            
+            // choose a photo from library
+            self.imagePicker.sourceType = .PhotoLibrary
+            self.presentViewController(self.imagePicker, animated: true, completion: nil)
+        })
+        let albumAction = UIAlertAction(title: "Photo from Album", style: .Default, handler: {(albumAction) -> Void in
+            
+            // choose a photo from album
+            self.imagePicker.sourceType = .SavedPhotosAlbum
+            self.presentViewController(self.imagePicker, animated: true, completion: nil)
+        })
+        
+        alert.addAction(cameraAction)
+        alert.addAction(libraryAction)
+        alert.addAction(albumAction)
+        
+        // present the controller
+        self.presentViewController(alert, animated: true, completion: nil)
     }
-    */
-
+    
+//    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?)
+//    {
+//        if let pickedImage = editingInfo![UIImagePickerControllerOriginalImage] as? UIImage
+//        {
+//            // fit the image to the view and add it
+//            imageView.image = pickedImage
+//        }
+//        
+//        dismissViewControllerAnimated(true, completion: nil)
+//    }
+    
+    // finished picking the image from the controller
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject])
+    {
+        if let pickedImage = info[UIImagePickerControllerEditedImage] as? UIImage
+        {
+            // fit the image to the view and add it
+            imageView.image = pickedImage
+        }
+        else if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage
+        {
+            // fit the image to the view and add it
+            imageView.image = pickedImage
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // just get rid of the imagePickerView if user cancels
+    func imagePickerControllerDidCancel(picker: UIImagePickerController)
+    {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // begin Songage creation. Send imageData to Clarifai
+    @IBAction func createSongageTapped(sender: UIButton)
+    {
+        
+    }
+    
 }
